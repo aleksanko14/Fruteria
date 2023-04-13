cdb8defd16462d5a1f81e846d70eb5f216b10e59
Aleksandr <alexanderkosenko@gmail.com>
2023-04-13 14:37:16 +0200

    En proceso

--- javascript.js
+++ javascript.js
@@ -1,3 +1,37 @@
-function okey(){
-    console.log("Pefe")
+function producto(fruta){
+    console.log(fruta)
+}
+
+function accion1(){
+    console.log("Flama")
+}
+
+function accion2(){
+    console.log("Flama")
+}
+
+function accion3(){
+    console.log("Flama")
+}
+
+function accion4(){
+    console.log("De lujo")
+}
+//-----------------------------------------------------------------//
+
+const tFruta= document.getElementById('fruta')
+const tPrecio= document.getElementById('precio')
+const tpeso= document.getElementById('peso')
+const tResult= document.getElementById('precioFinal')
+
+let pManzana = {
+    nombre: "Manzana",
+    precio: "1.60"
+}
+
+function producto(fruta){
+    tFruta.innerHTML=fruta;
+    if (fruta){
+        tPrecio.innerHTML=pManzana;
+    }
 }
