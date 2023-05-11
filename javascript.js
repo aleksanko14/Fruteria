353c4efc319e4e981f443842c9700f31edf58cdd
Aleksandr <alexanderkosenko@gmail.com>
2023-05-11 10:06:04 +0200

    Todo arreglado en javascript, falta retocar css

--- javascript.js
+++ javascript.js
@@ -104,6 +104,7 @@ let pesoRandom = 0;
 let sigo = true;
 let noSigo = false;
 let initialValue = 0;
+let acumularImporte = 0;
 
 //Ver datos de Frutas
 function producto(fruta) {
@@ -125,8 +126,8 @@ function pesar() {
 
   arrayDatosFruta.forEach((fruta) => {
     if (fruta.nombre == frutaGuardada) {
-      fruta.peso = pesoRandom;
-      fruta.importe = (pesoRandom * fruta.precio).toFixed(2);
+      fruta.peso = parseFloat(pesoRandom);
+      fruta.importe = parseFloat((pesoRandom * fruta.precio).toFixed(2));
       dResult.innerHTML = fruta.importe;
     }
   });
@@ -145,12 +146,12 @@ function limpiar(){
   console.log(arrayDatosFruta)
 
   dFruta.innerHTML= "";
-  dPrecio.innerHTML= null;
-  dPeso.innerHTML= null;
-  dResult.innerHTML= null;
+  dPrecio.innerHTML= "";
+  dPeso.innerHTML= "";
+  dResult.innerHTML= "";
 
-  tDisplay.innerHTML = null;
-  importeTotal.innerHTML = null;
+  tDisplay.innerHTML = "";
+  importeTotal.innerHTML = "";
 }
 
 //Exportar datos al ticket (Comprar) 
@@ -161,22 +162,21 @@ function comprar(){
     if (fruta.nombre == frutaGuardada) {
       tDisplay.innerHTML += `<tr><td>${fruta.nombre}</td><td>${fruta.precio}</td><td>${fruta.peso}</td><td>${fruta.importe}</td></tr>`
 
+      acumularImporte = arrayDatosFruta.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.importe,
+        initialValue
+      );
+      
+      importeTotal.innerHTML = `<td colspan="4" class='importeTotalCompra'>TOTAL: ${parseFloat(acumularImporte).toFixed(2)}€</td>`
     }
 
     
   });
-  // // const sumWithInitial = arrayDatosFruta.reduce(
-  // //   (accumulator, currentValue) => accumulator + currentValue.importe,
-  // //   initialValue
-  // );
+ 
 
-  const suma1 = arrayDatosFruta.reduce((valorAnterior, valorActual) => {
-    return valorAnterior.importe + valorActual;
-  }, 0);
   
-  console.log(suma1);
 
-    importeTotal.innerHTML = `<td colspan="4" class='importeTotalCompra'>TOTAL: ${parseFloat(sumWithInitial).toFixed(2)}€</td>`
+
 
 
 }
